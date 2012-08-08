88217e6e7fe4b8a2467de4795700dc7ae384311d
tslmy <tslimingyang@126.com>
2012-08-08 18:29:58 +0800

    First commit!

---
+++ README.md
@@ -0,0 +1,28 @@
+Grum: _convert __G__oogle __R__eader __u__nread articles to __M__arkdown text files!_
+=============
+
+usage
+------
+Run reader.py.   
+_(You may also want to edit some account information in it to simplify personal use in the long run.)_
+
+author
+------
+[tslmy](http://tslmy.tk).
+
+LICENSE? Not important.
+
+file list
+---------
+1. feedparser.py2. html2text.py3. reader.py
+As the program runs, the following file will be generated. Relax, they are simply caches. Harmless.
+1. html2text.pyc
+2. reader.pyc
+credits
+-------
+###[html2text](https://github.com/aaronsw/html2text/)
+
+Thanks, [aaronsw](https://github.com/aaronsw)! Your effort enabled Grum to convert XML to Markdown-syntaxed text!
+
+###[feedparser](http://code.google.com/p/feedparser/)
+Thank [Ade Oshineye](https://plus.google.com/105037104815911535953/posts) for parsing ATOM feeds!
---
+++ reader.py
@@ -0,0 +1,60 @@
+#
+#Informations that you need to edit!!
+EMAIL = ''   #Your Google ID(E-mail Address) here.
+PASSWORD = ''        #Here goes your password for the mentioned Google ID.
+ENTRY_AMOUNT = 20             #(Optional)Amount of the entries you would like me to check.
+from urllib import urlencode
+from urllib2 import urlopen, Request
+import feedparser, html2text #under the same folder
+not_for_filename=[u'\\',u'/',u':',u'*',u'?',u'”',u'<',u'>',u'|']
+def standardize_file_name(original_filename):
+	filename=original_filename
+	for character in not_for_filename:
+		filename=filename.replace(character,'')
+	return filename
+#In case of anyone who don't read English.
+if EMAIL=='':
+	EMAIL=raw_input   ('Email(Google ID):')
+if PASSWORD=='':
+	PASSWORD=raw_input('Password for it :')
+#getting Authorization
+request = Request('https://www.google.com/accounts/ClientLogin', urlencode({
+    'service': 'reader',
+    'Email': EMAIL,
+    'Passwd': PASSWORD
+}))
+print '>Authorizing...'
+f = urlopen(request)
+lines = f.read().split()
+auth = lines[2][5:]
+headers = {'Authorization': 'GoogleLogin auth=' + auth}
+# request = Request('https://www.google.com/reader/api/0/subscription/list?output=json', headers=headers)
+# f = urlopen(request)
+# subscription_list = f.read()
+request = Request('https://www.google.com/reader/atom/user/-/state/com.google/reading-list?n='+str(ENTRY_AMOUNT)+'&xt=user/-/state/com.google/read', headers=headers)
+#Use "-" to replace the USER_ID, and Google will replace "-" according to the user that the header described.
+f = urlopen(request)
+atom = f.read()
+d = feedparser.parse(atom)
+last_updated_time = d.feed.updated_parsed
+print 'Linked to '+d.feed.title+', last updated at '+d.feed.updated+'.'
+for entry in d.entries:
+    print entry.updated+u'>'+entry.title#+u': '+entry.summary
+    try:
+        this_summary = entry.summary
+    except AttributeError:
+        this_summary = ''
+    try:
+        this_content = entry.content[0]['value']
+    except AttributeError:
+        this_content = ''
+	#from summary and content, select the longer one.
+    if len(this_summary)>len(this_content):
+        content_HTML=this_summary
+    else:
+        content_HTML = this_content
+    content_Markdown = html2text.html2text(content_HTML)
+    f=open(standardize_file_name(entry.title)+'.txt','w+')
+    f.write(content_Markdown.replace('\n','\r\n').encode('gb2312','ignore'))#Chinese uses GB code.//Windows uses \r\n.
+    f.close()
+#print '============ '+str(len(d.entries))+' entries in total =============='
